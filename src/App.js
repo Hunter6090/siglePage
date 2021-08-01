f71e671f9f08914d5aad9760a24b6551ecb50819
HUN7 <rockofsky1@gmail.com>
2021-08-01 14:31:04 +0800

    2021-08-01: 1.Base template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './page/Home';
-import About from './page/About';
-import Contact from './page/Contact';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
     <Router>
         <div>
+        {/*}
           <h2>Welcome to React Router Tutorial</h2>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <ul className="navbar-nav mr-auto">
@@ -25,6 +27,15 @@ class App extends Component {
               <Route path='/contact' component={Contact} />
               <Route path='/about' component={About} />
           </Switch>
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-secondary"><Link to={'/'} className="nav-link"> Home </Link></button>
+            <button type="button" class="btn btn-secondary">Middle</button>
+            <button type="button" class="btn btn-secondary">Right</button>
+          </div>
+        */}
+
+
+          <Navbar />
         </div>
       </Router>
     );
